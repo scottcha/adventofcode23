e7f519aec0a630038303051a2db70a3b382c70a7
scottcha <scottcha@live.com>
2023-12-05 18:22:36 -0700

    Parallelize the bottleneck for 75% speedup

--- day5_part2/src/main.rs
+++ day5_part2/src/main.rs
@@ -3,16 +3,23 @@ use std::io::{self, BufRead};
 use std::path::Path;
 use std::time::Instant;
 use rayon::prelude::*;
+use rayon::ThreadPoolBuilder;
 
 // pre optimization
 // lowest seed id: 2254686
 // highest seed id: 4102374000
 // Time elapsed is: 100.8056041s
+// 4 threads debug Time elapsed is: 380.5795926s
+// 12 threads debug Time elapsed is: 193.5838783s
+// 20 threads debug Time elapsed is: 170.1574675s
+// 24 threads debug Time elapsed is: 166.9757496s
+// 24 threads release Time elapsed is: 28.5595146s
 fn main() -> io::Result<()>{
 
     let start = Instant::now();
-    //let path = Path::new("day5_part1_input.txt");
+
     let path = Path::new("day5_part2_input.txt");
+    //let path = Path::new("day5_part2_test_input.txt");
     let file = File::open(&path)?;
     let mut reader = io::BufReader::new(file);
 
@@ -102,24 +109,28 @@ fn update_mapping(mapping: &mut Vec<(u64, u64, u64)>, seed_ids: &mut Vec<u64>) {
     //sort the mapping by the first element
     mapping.sort_by(|a, b| a.0.cmp(&b.0));
     //for each seed id, do the mapping
-    for seed_id in seed_ids
-    {
-        //go through the mapping and find the appropriate range if one exist
-        for map in &*mapping
-        {   
-            //println!(" checking seed_id: {} with map: {:?}", *seed_id, map);
-            if *seed_id >= map.0 && *seed_id < map.0 + map.2
-            {
-                //println!("found range");
-                let offset = *seed_id - map.0;
-                let new_id = map.1 + offset;
-                //println!("mapping seed_id: {} to new_id: {}", *seed_id, new_id);
-                *seed_id = new_id;
-                break;
+    //for seed_id in seed_ids
+    let pool = ThreadPoolBuilder::new().num_threads(24).build().unwrap();
+    pool.install(|| {
+        seed_ids.par_iter_mut().for_each(|seed_id|
+        {
+            //go through the mapping and find the appropriate range if one exist
+            for map in &*mapping
+            {   
+                //println!(" checking seed_id: {} with map: {:?}", *seed_id, map);
+                if *seed_id >= map.0 && *seed_id < map.0 + map.2
+                {
+                    //println!("found range");
+                    let offset = *seed_id - map.0;
+                    let new_id = map.1 + offset;
+                    //println!("mapping seed_id: {} to new_id: {}", *seed_id, new_id);
+                    *seed_id = new_id;
+                    break;
+                }
             }
-        }
-        //since default mapping is 1:1, we don't need to do anything if no range is found 
-    }
+            //since default mapping is 1:1, we don't need to do anything if no range is found 
+        });
+    });
     //clear the map and start a new one
     mapping.clear();
 }
